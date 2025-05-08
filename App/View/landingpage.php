8e3189ce49216904032f7986d0042efe80b15e9c
evertonrafaelifsp <evertonrafael@ifsp.edu.br>
2025-05-08 11:56:51 -0300

    Atualização do Framework para aceitar Rotas via Banco de Dados

---
+++ App/View/landingpage.php
@@ -0,0 +1,10 @@
+        <?php 
+        
+        include "includes/landingpage/header.php"; 
+        include "includes/landingpage/menu.php"; 
+        $this->content(); 
+        include "includes/landingpage/footer.php"; 
+        
+        ?>
+        <!-- Footer-->
+        
